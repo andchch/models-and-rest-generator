446fc8f50cb76856040686693250fe909c91d345
Andrey Martinich <aimartinich@edu.hse.ru>
2024-07-21 00:42:07 +0400

    Methods done

--- model_generator.py
+++ model_generator.py
@@ -1,6 +1,6 @@
 import json
+import os.path
 from typing import Dict
-import time
 from jinja2 import FileSystemLoader, Environment
 
 
@@ -11,6 +11,10 @@ base_types = {
     'boolean': 'bool',
     'array': 'List[Any]',
     'dict': 'Dict[Any]',
+    'array.string': 'List[str]',
+    'array.integer': 'List[int]',
+    'array.number': 'List[float]',
+    'array.boolean': 'List[bool]',
 }
 
 keywords = {
@@ -40,12 +44,30 @@ def parse_schema(schema: Dict, parent=None, path='') -> Dict[str, Dict]:
 
         for key, value in schema['properties'].items():
             current_path = f'{path}.{key}' if path else key
-            field_info = {
-                'type': value.get('type'),
-                'required': key in required_fields,
-                'parent': parent,
-                'path': current_path,
-            }
+            prop_type = value.get('type')
+            if prop_type == 'array':
+                if 'items' in value:
+                    prop_type += '.' + value['items'].get('type')
+                field_info = {
+                    'type': prop_type,
+                    'required': key in required_fields,
+                    'parent': parent,
+                    'path': current_path,
+                }
+            elif value.get('properties') == {}:
+                field_info = {
+                    'type': 'dict',
+                    'required': key in required_fields,
+                    'parent': parent,
+                    'path': current_path,
+                }
+            else:
+                field_info = {
+                    'type': value.get('type'),
+                    'required': key in required_fields,
+                    'parent': parent,
+                    'path': current_path,
+                }
 
             for keyword, val in keywords.items():
                 if keyword in value:
@@ -107,28 +129,12 @@ def get_depth(parsed_data):
     return levels
 
 
-def sort_models_by_dependencies(models: Dict, properties: Dict) -> Dict:
-    combined_items = [
-        (key, models[key], properties.get(key, float('-inf')))
-        for key in models
-    ]
-    sorted_items = sorted(
-        combined_items, key=lambda item: item[2], reverse=True
-    )
-    sorted_models = {key: value for key, value, _ in sorted_items}
-    return sorted_models
-
-
-if __name__ == '__main__':
-    start = time.perf_counter()
-    schema = load_json_schema('test_polygon/test_schema.json')
-    s = parse_schema(schema)
-
+def sort_models_by_dependencies(parsed_data, properties: Dict) -> Dict:
     models = {}
-    for path, info in s.items():
+    for path, info in parsed_data.items():
         parts = path.split('.')
         if len(parts) == 1:
-            class_name = s.get('Kind', 'Root')
+            class_name = parsed_data.get('Kind', 'Root')
             field_name = parts[0]
         else:
             class_name = (
@@ -142,16 +148,33 @@ if __name__ == '__main__':
             models[class_name] = []
         models[class_name].append(generate_field_code(field_name, info))
 
-    a = sort_models_by_dependencies(models, get_depth(s))
+    combined_items = [
+        (key, models[key], properties.get(key, float('-inf')))
+        for key in models
+    ]
+    sorted_items = sorted(
+        combined_items, key=lambda item: item[2], reverse=True
+    )
+    sorted_models = {key: value for key, value, _ in sorted_items}
+    return sorted_models
 
-    templateLoader = FileSystemLoader(searchpath='./templates')
-    templateEnv = Environment(loader=templateLoader)
+
+def generate_pydantic_models(
+    json_schema: str, out_dir: str = 'models'
+) -> None:
+    schema = load_json_schema(json_schema)
+    parsed_data = parse_schema(schema)
+    models = sort_models_by_dependencies(parsed_data, get_depth(parsed_data))
+
+    template_loader = FileSystemLoader(searchpath='./templates')
+    template_env = Environment(loader=template_loader)
     template_file = 'pydantic_class.jinja2'
-    template = templateEnv.get_template(template_file)
-    code = template.render(models=a)
+    template = template_env.get_template(template_file)
+    code = template.render(models=models)
 
-    with open('models/model.py', 'w') as file:
-        file.write(code)
+    with open(os.path.join(out_dir, 'model.py'), 'w') as out_file:
+        out_file.write(code)
 
-    finish = time.perf_counter()
-    print('Время работы: ' + str(finish - start))
+
+if __name__ == '__main__':
+    generate_pydantic_models('test_polygon/test_schema1.json')
